8be7e20cf3b4c4296af81333069b351420571747
czj <czj@study.com>
2022-03-07 19:25:33 +0800

    Second

--- src/com/aaa/demo1/q.java
+++ src/com/aaa/demo1/q.java
@@ -2,7 +2,9 @@ package com.aaa.demo1;
 
 public class q {
     public static void main(String[] args) {
-        int a;
+        int a = 5;
         System.out.println(8.85*3.00);
+        System.out.println(8.85*3.00*a);
+        System.out.println(8.85*3.00*a*a);
     }
 }
